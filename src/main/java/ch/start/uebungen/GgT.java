0b533634576e40bfd07ef22e52ea96a1daba3cf2
NadineEisenring <59962418+NadineEisenring@users.noreply.github.com>
2020-04-07 12:58:35 +0200

    Update GitRebaseUebung.md

--- src/main/java/ch/start/uebungen/GgT.java
+++ src/main/java/ch/start/uebungen/GgT.java
@@ -1,22 +1,80 @@
-package main.java.ch.start.uebungen;
+package ch.start.uebungen;
+
+import java.util.Scanner;
 
 public class GgT {
+	
+	static public final Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("------ Start der Berechnung des GgTs -------");
+		GgT ggt = new GgT();
+		int ersteZahl; //erste Zahl die eingelesen wird
+		int zweiteZahl; //zweite Zahl die eingelesen wird
+		int kleineZahl; //kleinere der beiden Zahlen
+		int grosseZahl; //grössere der beiden Zahlen
+		int resultat = 0;
+		
+		System.out.println("------ Start der Berechnung des GgTs -------");		
 		
 //		System.out.println("Welche Argumente haben wir: ");
 //		for(int i=0; i<args.length; i++) {
 //			System.out.println(i+": "+args[i]);
 //		}
 		
+		
 		if(args.length>0) {
-			GgTMethod methodToUse = GgTMethod.valueOf(args[0]);
+			GgTMethod methodToUse = GgTMethod.valueOf(args[0]); //klassisch [0], modern [1]
 			System.out.println("Verwende: " + methodToUse);
+			
+			System.out.println("\nUm den GgT auszurechnen, gib zwei Zahlen an.");
+
+//			Einlesen erste Zahl
+			System.out.println("\nErste Zahl: ");
+			ersteZahl = Integer.parseInt(s.nextLine());
+			
+//			Einlesen zweite Zahl, so lange bis zweite Zahl nicht gleich erste Zahl		
+			do {
+				System.out.println("\nZweite Zahl: ");
+				zweiteZahl = Integer.parseInt(s.nextLine());
+				if (ersteZahl == zweiteZahl) {
+					System.out.println("\nDie Zahlen sind identisch. Probiere es erneut.");				
+				}
+			}while (zweiteZahl == ersteZahl);
+			
+//			Herausfinden welche Zahl ist kleiner/grösse
+			if (ersteZahl > zweiteZahl) {
+				grosseZahl = ersteZahl;
+				kleineZahl = zweiteZahl;
+			}else {
+				grosseZahl = zweiteZahl;
+				kleineZahl = ersteZahl;
+			}
+			
+//			Überprüfen, welche methode verwendet wird
+			switch(methodToUse) {
+			  case KLASSISCHER_EUKLIDES:
+				//berechnet den ggt
+				while(grosseZahl > kleineZahl) {
+					resultat = grosseZahl - kleineZahl;
+					grosseZahl = resultat;
+				}
+			    break;
+			  case MODERNER_EUKLIDES:
+				while(grosseZahl > kleineZahl) {
+					resultat = grosseZahl % kleineZahl;
+					grosseZahl = resultat;
+				}
+			    break;
+			}
+			
+//			zweite Möglichkeit
+//			if(methodToUse == GgTMethod.KLASSISCHER_EUKLIDES){				
+//			} 
+			
+			System.out.println("\nDer grösste gemeinsame Teiler lautet: " + resultat);
 			
 		}
 		
-
 		
 		System.out.println("------ Ende der Berechnung des GgTs -------");
 	}
